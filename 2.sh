58d2eda5564640f10410c1f380c09860f7b613d9
awinecki <winecki@gmail.com>
2021-07-11 01:33:06 +0200

    1st commit

---
+++ 2.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+. "$(command -v import)"
+
+import "https://gist.githubusercontent.com/TooTallNate/ed7a72112acea4fab96bad1dde8ce5bf/raw/250d8574f55bf05adf5ac4c007f48726992e453f/add.sh"
+
+add 20 22
