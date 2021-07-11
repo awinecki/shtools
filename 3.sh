bcd4b658be9a0ee91fef4134a88eb1ab04a07612
awinecki <winecki@gmail.com>
2021-07-11 02:20:19 +0200

    test

--- 3.sh
+++ 3.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-
 . "$(command -v import)"
-
-import "awinecki/shtools/log.sh@1e59caf"
+import "awinecki/shtools/log.sh@main"
 
 log err "oh no sth went wrong!"
 log info "just a sample message"
--- log.sh
+++ log.sh
@@ -1,7 +1,8 @@
-# source "./datetime.sh"
 import "./datetime.sh"
 
+# $1: log msg type
+# $2: log msg body
 log() {
 	type="$(echo $1 | tr '[:lower:]' '[:upper:]')"
-	echo "[$(datetime)][${type}] $2"
+	echo "[$(datetime)][${type}] $2 (test)"
 }
