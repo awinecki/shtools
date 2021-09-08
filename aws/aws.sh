0356afc2db7f02c06b70fd82e330aad834fe77c6
awinecki <winecki@gmail.com>
2021-09-08 22:31:23 +0200

    test

--- aws/aws.sh
+++ aws/aws.sh
@@ -2,10 +2,14 @@
 # import ../validate.sh || source $(dirname "$0")/../validate.sh
 . "$(command -v import)"
 [[ -n $LOCAL ]] && source $(dirname $0)/../validate.sh || import ../validate.sh
+# [[ -n $LOCAL ]] && source $(dirname $0)/../log.sh || import ../log.sh
 
 function aws_regions() { set -e
+  # log info "listing aws regions.."
   binary_exists aws
 	aws ec2 describe-regions --region us-east-1 --output text | cut -f4
+  # log info "done!"
+  echo "DONE"
 }
 
 aws_regions
--- log.sh
+++ log.sh
@@ -1,4 +1,7 @@
-import "./datetime.sh"
+#!/usr/bin/env bash
+. "$(command -v import)"
+# import "./datetime.sh"
+[[ -n $LOCAL ]] && source ./$(dirname $0)/datetime.sh || import ./datetime.sh
 
 # $1: log msg type
 # $2: log msg body
