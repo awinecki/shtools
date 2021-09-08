e2ea92ea938d358a562ecbb7bb7e40e31d6c8900
awinecki <winecki@gmail.com>
2021-09-09 01:11:10 +0200

    aws functions

--- aws.sh
+++ aws.sh
@@ -5,12 +5,17 @@ get ./validate.sh
 get ./log.sh
 
 function aws_regions() { set -e
-  log info "listing aws regions.."
   binary_exists aws
-	aws ec2 describe-regions --region us-east-1 --output text | cut -f4
-  log info "done!"
-  log warn "ITS WORKING!!!"
-  echo "DONE"
+	aws ec2 describe-regions --output text | cut -f4
 }
 
-aws_regions
+function list_ec2_in_all_regions() { set -e
+  binary_exists aws
+  binary_exists jq
+  for region in $(aws_regions); do
+    log info "Listing ec2 instances in region $region.."
+    aws ec2 describe-instances --region $region \
+      | jq '.Reservations[].Instances[].Tags[] | select(.Key == "Name") | .Value'
+    echo -e "\n"
+  done
+}
