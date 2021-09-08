7456c36b56028359b934f79d6c4cd61b577f93fb
awinecki <winecki@gmail.com>
2021-09-09 01:38:42 +0200

    clean up sample scripts

--- 1.sh
+++ 1.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 . "$(command -v import)"
 
-import "awinecki/shtools/aws.sh"
-
 # The URL is downloaded once, cached forever, and then sourced
 # import "https://git.io/fAWiz"
+# import "https://gist.githubusercontent.com/TooTallNate/ed7a72112acea4fab96bad1dde8ce5bf/raw/250d8574f55bf05adf5ac4c007f48726992e453f/add.sh"
+import "awinecki/shtools/aws.sh"
+
 
 aws_regions
-# 42
