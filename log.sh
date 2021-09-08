f684374214d9e327ae51c01210ab19b4d4091fed
awinecki <winecki@gmail.com>
2021-09-09 01:10:52 +0200

    log.sh color support

--- log.sh
+++ log.sh
@@ -2,10 +2,20 @@
 function get() { set -e; dep=$1; [[ -n $LOCAL ]] && source $(dirname $0)/$dep || import $dep; } ; . "$(command -v import)";
 
 get ./datetime.sh
+get ./colors.sh
 
 # $1: log msg type
 # $2: log msg body
 log() {
-	type="$(echo $1 | tr '[:lower:]' '[:upper:]')"
-	echo "[$(datetime)][${type}] $2"
+	local type="$(echo $1 | tr '[:lower:]' '[:upper:]')"
+  if [[ $type = "INFO" ]]; then
+    local msg_color=$blue
+  elif [[ $type = "WARN" ]]; then
+    local msg_color=$yellow
+  elif [[ $type = "ERR" ]]; then
+    local msg_color=$red
+  else
+    local msg_color=""
+  fi
+	echo "${msg_color}[$(datetime)][${type}] $2${reset}"
 }
