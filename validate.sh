569487c47548c4993287f8f6db85ca36d539df1e
awinecki <winecki@gmail.com>
2021-09-09 01:16:46 +0200

    allow binary_exists to check for multiple commands

--- validate.sh
+++ validate.sh
@@ -1,9 +1,9 @@
 function binary_exists() {
-  cmd=$1
-	if ! command -v $cmd &> /dev/null
-		then
-			echo "[ERR] Command $cmd could not be found!"
-    exit
-	fi
+  for cmd in $@; do
+    if ! command -v $cmd &> /dev/null
+      then
+        echo "[ERR] Command $cmd could not be found!"
+      exit 1
+    fi
+  done
 }
-
